5c6b64583b5227a407c7333a361d1da9a04912b2
Margareth <magasasaki@gmail.com>
2023-03-15 20:03:45 -0300

    primeiro commit

---
+++ app/src/main/java/projetocalculadoraemjava/Calculadora.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projetocalculadoraemjava;
+
+/**
+ *
+ * @author Admin
+ */
+public class Calculadora {
+    public double somar(double a, double b){
+        return a + b;
+    }
+    public double subtrair(double a, double b){
+        return a - b;
+}
+     public double dividir(double a, double b){
+        return a / b;
+    
+}
+      public double multiplicar(double a, double b){
+        return a * b;
+     
+}
+      
+}
